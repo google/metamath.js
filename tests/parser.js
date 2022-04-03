97c467026f655bff96480d694c249bd467894b4a
Sam Goto <samuelgoto@gmail.com>
2022-04-02 17:56:28 -0700

    Add nearley as a dependency and build a simple helloworld parser

--- tests/parser.js
+++ tests/parser.js
@@ -1,7 +1,35 @@
 const Assert = require("assert");
+const nearley = require("nearley");
+const compile = require("nearley/lib/compile");
+const generate = require("nearley/lib/generate");
+const nearleyGrammar = require("nearley/lib/nearley-language-bootstrapped");
 
 describe("Parser", () => {
-  it("Basic", () => {
+  function compileGrammar(sourceCode) {
+    // Parse the grammar source into an AST
+    const grammarParser = new nearley.Parser(nearleyGrammar);
+    grammarParser.feed(sourceCode);
+    const grammarAst = grammarParser.results[0]; // TODO check for errors
+      
+    // Compile the AST into a set of rules
+    const grammarInfoObject = compile(grammarAst, {});
+    // Generate JavaScript code from the rules
+    const grammarJs = generate(grammarInfoObject, "grammar");
+      
+    // Pretend this is a CommonJS environment to catch exports from the grammar.
+    const module = { exports: {} };
+    eval(grammarJs);
+
+    return module.exports;
+  }
+
+  it("helloworld", () => {    
+    const grammar = compileGrammar(`
+      main -> foo | bar | "helloworld"
+    `);
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+    parser.feed("helloworld");
+    assertThat(parser.results).equalsTo([["helloworld"]]);
   });
 });
 
