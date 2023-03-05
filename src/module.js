9934dfdf2d6e9f754608149b62a300448a04c8a5
Sam Goto <samuelgoto@gmail.com>
2023-03-05 09:25:29 -0800

    Add a module.js file to the static folder that serves all of the necessary parts to run in the browser

---
+++ src/module.js
@@ -0,0 +1,9 @@
+const parser = require("./parser.js");
+const metamath = require("./metamath.js");
+const descent = require("./descent.js");
+
+module.exports = {
+  parser: parser,
+  metamath: metamath,
+  descent: descent,
+};
--- static/module.js
+++ static/module.js
@@ -911,7 +911,7 @@
 }));
 
 }).call(this)}).call(this,require('_process'),"/node_modules/nearley/lib")
-},{"./nearley":5,"./nearley-language-bootstrapped.js":4,"_process":11,"fs":9,"path":10}],3:[function(require,module,exports){
+},{"./nearley":5,"./nearley-language-bootstrapped.js":4,"_process":13,"fs":11,"path":12}],3:[function(require,module,exports){
 (function(root, factory) {
     if (typeof module === 'object' && module.exports) {
         module.exports = factory(require('./nearley'));
@@ -1858,6 +1858,304 @@ if (typeof module !== 'undefined'&& typeof module.exports !== 'undefined') {
 }));
 
 },{}],6:[function(require,module,exports){
+const {lexicon} = require("../src/lexer.js");
+const {MM} = require("../src/metamath.js");
+const moo = require("moo");
+
+function *tokens(code) {
+  const lexer = moo.compile(lexicon);
+  lexer.reset(code);
+  do {
+    const next = lexer.next();
+    if (!next) {
+      return;
+    }
+    yield next;
+  } while (true);
+}
+
+class Parser {
+  constructor(handler) {
+    this.handler = handler;
+  }
+  parse(tokens) {
+    this.it = tokens;
+    this.head = this.it.next();
+    return this.program();
+  }
+  eat(type) {
+    if (this.head.value.type != type) {
+      throw new Error(`Expected ${type} got ${this.head.value.type}.`);
+    }
+    const token = this.head.value;
+    this.head = this.it.next();
+    return token.value;
+  }
+  accepts(type) {
+    if (this.done()) {
+      return false;
+    }
+    if (this.head.value.type == type) {
+      return true;
+    }
+    return false;
+  }
+  done() {
+    return this.head.done;
+  }
+  ws() {
+    this.eat("ws");
+  }
+  comment() {
+    this.eat("comment");
+  }
+  v() {
+    const v = this.eat("v");
+    this.space();
+    const symbols = [];
+    do {
+      symbols.push(this.eat("sequence"));
+      this.space();
+    } while (this.accepts("sequence"));
+    this.eat("dot");
+    this.dispatch([v, symbols]);
+  }
+  c() {
+    const c = this.eat("c");
+    this.space();
+    const symbols = [];
+    do {
+      symbols.push(this.eat("sequence"));
+      this.space();
+    } while (this.accepts("sequence"));
+    const dot = this.eat("dot");
+    this.dispatch([c, symbols]);
+  }
+  dispatch(e) {
+    if (this.handler) {
+      this.handler.feed(e);
+    }
+  }
+  d() {
+    const d = this.eat("d");
+    this.space();
+    const symbols = [];
+    do {
+      symbols.push(this.eat("sequence"));
+      this.space();
+    } while (this.accepts("sequence"));
+    this.eat("dot");
+    this.dispatch([d, symbols]);
+  }
+  space(optional = false) {
+    do {
+      if (this.accepts("ws")) {
+        this.ws();
+      } else if (this.accepts("comment")) {
+        this.comment();
+      } else if (!optional) {
+        throw new Error(`Excepted a ws or a comment`);
+      }
+    } while (this.accepts("ws") || this.accepts("comment"));
+  }
+  f(label) {
+    const f = this.eat("f");
+    this.space();
+    const type = this.eat("sequence");
+    this.space();
+    const varz = this.eat("sequence");
+    this.space();
+    this.eat("dot");
+    this.dispatch([label, f, type, varz]);
+  }
+  e(label) {
+    const e = this.eat("e");
+    this.space();
+    const varz = this.eat("sequence");
+    this.space();
+    const sequence = [];
+    do {
+      sequence.push(this.eat("sequence"));
+      this.space();
+    } while (this.accepts("sequence"));
+    this.eat("dot");
+    this.dispatch([label, e, varz, sequence]);
+  }
+  a(label) {
+    const a = this.eat("a");
+    this.space();
+    const type = this.eat("sequence");
+    this.space();
+    const sequence = [];
+    while (this.accepts("sequence")) {
+      sequence.push(this.eat("sequence"));
+      this.space();
+    };
+    this.eat("dot");
+    this.dispatch([label, a, type, sequence]);
+  }
+  value() {
+    return this.head.value.value;
+  }
+  compressed() {
+    const p1 = this.eat("sequence"); // (
+    this.space();
+    const local = [];
+    while (this.value() != ")") {
+      local.push(this.eat("sequence"));
+      this.space();
+    };
+    const p2 = this.eat("sequence"); // )
+    this.space();
+    const integers = [];
+    do {
+      integers.push(this.eat("sequence"));
+      this.space();
+    } while (this.accepts("sequence"));
+    return [p1, local, p2, integers.join("")];
+  }
+  uncompressed() {
+    const proof = [];
+    do {
+      proof.push(this.eat("sequence"));
+      this.space();
+    } while (this.accepts("sequence"));
+    return proof;
+  }
+  proof() {
+    const proof = this.eat("proof");
+    this.space();
+    if (this.value() == "(") {
+      return [proof, this.compressed()];
+    } else {
+      return [proof, this.uncompressed()];
+    }
+  }
+  p(label) {
+    const p = this.eat("p");
+    this.space();
+    const type = this.eat("sequence");
+    this.space();
+    const sequence = [];
+    do {
+      sequence.push(this.eat("sequence"));
+      this.space();
+    } while (this.accepts("sequence"));
+    const [proof, steps] = this.proof();
+    this.eat("dot");
+    this.dispatch([label, p, type, sequence, proof, steps]);
+  }
+  label() {
+    const label = this.eat("sequence");
+    this.space();
+    if (this.accepts("f")) {
+      this.f(label);
+    } else if (this.accepts("e")) {
+      this.e(label);
+    } else if (this.accepts("a")) {
+      this.a(label);
+    } else if (this.accepts("p")) {
+      this.p(label);
+    } else {
+      this.error();
+    }
+  }
+  error() {
+    // console.log(this.head);
+    const {line, col} = this.head.value;
+    throw new Error(`Syntax error on line ${line} column ${col}: unexpected ${this.head.value.type} token.`);
+  }
+  block() {
+    this.eat("lscope");
+    this.dispatch("push");
+    this.space();
+    while (!this.accepts("rscope")) {
+      this.statement();
+      this.space(true);
+    }
+    this.eat("rscope");
+    this.dispatch("pop");
+  }
+  file() {
+    this.eat("lfile");
+    this.space();
+    this.eat("sequence");
+    this.space();
+    this.eat("rfile");
+  }
+  statement() {
+    if (this.accepts("v")) {
+      this.v();
+    } else if (this.accepts("c")) {
+      this.c();
+    } else if (this.accepts("d")) {
+      this.d();
+    } else if (this.accepts("sequence")) {
+      this.label();
+    } else if (this.accepts("lscope")) {
+      this.block();
+    } else if (this.accepts("lfile")) {
+      this.file();
+    } else {
+      this.error();
+    }
+  }
+  program() {
+    this.space(true);
+    while (!this.done()) {
+      this.statement();
+      this.space(true);
+    }
+    return true;
+  }
+}
+
+function parse(code, handler) {
+  const parser = new Parser(handler);
+  const stream = tokens(code);
+  return parser.parse(stream);
+}
+
+function process(program) {
+  const mm = new MM();
+  mm.push();
+  
+  parse(program, {
+    feed(statement) {
+      if (statement == "push") {
+        mm.push();
+      } else if (statement == "pop") {
+        mm.pop();
+      } else {
+        mm.feed([statement]);
+      }
+    }
+  });
+
+  //const [, , proof] = mm.labels[label];
+  //return proof;
+  return mm;
+}
+
+class Verifier {
+  verify(program) {
+    let proofs = 0;
+    let mm = process(program);
+    for (const [, [, , proof]] of Object.entries(mm.labels).filter(([, [type]]) => type == "$p")) {
+      proof();
+      proofs++;
+    }
+    return proofs;
+  }
+}
+
+module.exports = {
+  parse: parse,
+  process: process,
+  Verifier: Verifier,
+};
+
+},{"../src/lexer.js":7,"../src/metamath.js":8,"moo":1}],7:[function(require,module,exports){
 const moo = require("moo");
 
 const lexicon = {
@@ -1891,7 +2189,7 @@ module.exports = {
   lexer: lexer
 };
 
-},{"moo":1}],7:[function(require,module,exports){
+},{"moo":1}],8:[function(require,module,exports){
 class Frame {
   constructor() {
     this.c = new Set();
@@ -2478,7 +2776,18 @@ module.exports = {
   MM: MM
 };
 
-},{}],8:[function(require,module,exports){
+},{}],9:[function(require,module,exports){
+const parser = require("./parser.js");
+const metamath = require("./metamath.js");
+const descent = require("./descent.js");
+
+module.exports = {
+  parser: parser,
+  metamath: metamath,
+  descent: descent,
+};
+
+},{"./descent.js":6,"./metamath.js":8,"./parser.js":10}],10:[function(require,module,exports){
 const nearley = require("nearley");
 const compile = require("nearley/lib/compile");
 const generate = require("nearley/lib/generate");
@@ -2642,9 +2951,9 @@ module.exports = {
   grammar: grammar,
 };
 
-},{"./lexer.js":6,"nearley":5,"nearley/lib/compile":2,"nearley/lib/generate":3,"nearley/lib/nearley-language-bootstrapped":4}],9:[function(require,module,exports){
+},{"./lexer.js":7,"nearley":5,"nearley/lib/compile":2,"nearley/lib/generate":3,"nearley/lib/nearley-language-bootstrapped":4}],11:[function(require,module,exports){
 
-},{}],10:[function(require,module,exports){
+},{}],12:[function(require,module,exports){
 (function (process){(function (){
 // 'path' module extracted from Node.js v8.11.1 (only the posix part)
 // transplited with Babel
@@ -3177,7 +3486,7 @@ posix.posix = posix;
 module.exports = posix;
 
 }).call(this)}).call(this,require('_process'))
-},{"_process":11}],11:[function(require,module,exports){
+},{"_process":13}],13:[function(require,module,exports){
 // shim for using process in browser
 var process = module.exports = {};
 
@@ -3363,5 +3672,5 @@ process.chdir = function (dir) {
 };
 process.umask = function() { return 0; };
 
-},{}]},{},[7,8])(8)
+},{}]},{},[9])(9)
 });
