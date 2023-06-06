73d3f733e3efebd1daebfa16d12550f907a35c89
MD SHAHIL <96812916+Shahil093@users.noreply.github.com>
2023-06-06 10:09:30 +0530

    Update lexer.js

--- src/lexer.js
+++ src/lexer.js
@@ -1,23 +1,23 @@
 /**
-  *  Copyright 2022 Google LLC
-  *
-  *  Licensed under the Apache License, Version 2.0 (the "License");
-  *  you may not use this file except in compliance with the License.
-  *  You may obtain a copy of the License at
-  *
-  *      https://www.apache.org/licenses/LICENSE-2.0
-  *
-  *  Unless required by applicable law or agreed to in writing, software
-  *  distributed under the License is distributed on an "AS IS" BASIS,
-  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  *  See the License for the specific language governing permissions and
-  *  limitations under the License.
-  **/
+ *  Copyright 2022 Google LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ **/
 
 const moo = require("moo");
 
 const lexicon = {
-  comment: {match: /\$\([\s]+(?:(?!\$\))[\s\S])*\$\)/, lineBreaks: true},
+  comment: { match: /\$\([\s]+(?:(?!\$\))[\s\S])*\$\)/, lineBreaks: true },
   lfile: "$[",
   rfile: "$]",
   v: "$v",
@@ -32,9 +32,9 @@ const lexicon = {
   // question: "?",
   lscope: "${",
   rscope: "$}",
-  //lparen: "(",
-  //rparen: ")",
-  ws: {match: /[\s]+/, lineBreaks: true},
+  // lparen: "(",
+  // rparen: ")",
+  ws: { match: /[\s]+/, lineBreaks: true },
   sequence: /[!-#%-~\?]+/,
   // letter_or_digit: /[A-Za-z0-9]/,
   // symbol: /[!-#%-~]+/,
@@ -44,5 +44,5 @@ const lexer = moo.compile(lexicon);
 
 module.exports = {
   lexicon: lexicon,
-  lexer: lexer
+  lexer: lexer,
 };
