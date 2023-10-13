97453c6c5f57c23bbb7d975ee6a42a76a1f2aff6
MD SHAHIL <96812916+Shahil093@users.noreply.github.com>
2023-10-13 14:23:00 +0530

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,24 +1,7 @@
-/**
- *  Copyright 2022 Google LLC
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- **/
-
 const nearley = require("nearley");
 const compile = require("nearley/lib/compile");
 const generate = require("nearley/lib/generate");
 const nearleyGrammar = require("nearley/lib/nearley-language-bootstrapped");
-const { lexer } = require("./lexer.js");
 
 function compileGrammar(handler = false, sourceCode) {
   // Parse the grammar source into an AST
